9bd2fcb5cc7b290fa7cd882ddabc321c9e41cd6d
Taylor <tbaustin1992@gmail.com>
2018-06-19 11:43:18 -0500

    update cart

--- src/App.js
+++ src/App.js
@@ -1,14 +1,76 @@
 import React, { Component } from 'react'
 import zygote from 'zygote-cart-v2'
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      show: false,
+      shouldRender: false,
+      class: ''
+    }
+  }
+
   componentDidMount() {
     zygote.site('goalrilla')
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log('PROPS', this.props, nextProps)
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (!prevState.show && this.state.show) {
+      this.setState({
+        shouldRender: true
+      })
+      setTimeout(() => {
+        this.setState({ class: 'showDiv' })
+      }, 0)
+    }
+    if (prevState.show && !this.state.show) {
+      setTimeout(() => {
+        this.setState({
+          shouldRender: false
+        })
+      }, 1000)
+      this.setState({ class: '' })
+    }
   }
 
   render() {
     return (
       <div>
+        <button
+          style={{
+            display: 'block',
+            border: 'none',
+            padding: '20px',
+            background: 'blue',
+            color: 'white',
+            fontSize: '20px',
+            margin: '10px',
+            cursor: 'pointer'
+          }}
+          onClick={() => this.setState({ show: !this.state.show })}
+        >
+          Toggle Div
+        </button>
+
+        {this.state.shouldRender ? (
+          <div className={`baseDiv ${this.state.class}`} />
+        ) : null}
+
+        <div
+          className={`secondDiv ${this.state.class}`}
+          style={{
+            width: '200px',
+            height: '200px',
+            background: '#999',
+            margin: '5px'
+          }}
+        />
         <button
           style={{
             display: 'block',
